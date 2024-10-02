58feda109a50b290a30803613d226bccf2d83c06
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-10-02 19:58:18 -0300

    Recriado Código com o JQuery
    - Upload JQuery v3.7.1;
    - Configurado Inserção do Endereço no Campo Endereço ao Pesquisar o CEP;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
-        // AJAX - Asynchronous JavaScript and XML
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('btn-buscar-cep').addEventListener('click', function() {
+//         // AJAX - Asynchronous JavaScript and XML
 
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value;
+//         const xhttp = new XMLHttpRequest();
+//         const cep = document.getElementById('cep').value;
+//         const endPoint = `https://viacep.com.br/ws/${cep}/json`;
+
+//         xhttp.open("GET", endPoint);
+//         xhttp.send();
+//     })
+// })
+
+$(document).ready(function() {
+    $('#btn-buscar-cep').click(function() {
+        const cep = $('#cep').val();
         const endPoint = `https://viacep.com.br/ws/${cep}/json`;
 
-        xhttp.open("GET", endPoint);
-        xhttp.send();
+        $.ajax(endPoint).done(function(resposta) {
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+        
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        
+            $('#endereco').val(endereco);
+        })
     })
 })
